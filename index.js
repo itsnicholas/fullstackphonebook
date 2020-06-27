0d9e0152b44ded160e31d4cd091c2cc79fe8f47c
itsnicholas <nicholas.danford@gmail.com>
2020-06-27 19:45:28 +0300

    3.4: puhelinluettelon backend step4

--- index.js
+++ index.js
@@ -47,7 +47,14 @@ let persons = [
       res.status(404).end()
     }
   })
-  
+
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+ 
+    response.status(204).end()
+  })
+
   const PORT = 3001
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
