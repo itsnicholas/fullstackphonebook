c221bba185cccc094c01389dd4e806347fb81772
itsnicholas <nicholas.danford@gmail.com>
2020-06-28 15:38:49 +0300

    3.8*: puhelinluettelon backend step8

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-app.use(express.json()) 
+app.use(express.json())
 
-app.use(morgan('tiny'))
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     { 
