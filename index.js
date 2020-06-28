00624fa24bbbae88c8583e46e7f9c8f7c6eb00ae
itsnicholas <nicholas.danford@gmail.com>
2020-06-28 22:29:45 +0300

    3.9 puhelinluettelon backend step9

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+
+app.use(cors())
 
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
