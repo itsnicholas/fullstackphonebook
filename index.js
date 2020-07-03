5818e755cd06f9418cd007b599a1435eb2d33fa6
itsnicholas <nicholas.danford@gmail.com>
2020-07-03 20:02:08 +0300

    3.19: puhelinluettelo ja tietokanta, step7

--- index.js
+++ index.js
@@ -116,6 +116,8 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
   
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+      return response.status(400).json({ error: error.message })
     }
   
     next(error)
--- models/person.js
+++ models/person.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
 mongoose.set('useFindAndModify', false)
+mongoose.set('useCreateIndex', true);
 
 const url = process.env.MONGODB_URI
 
@@ -15,10 +17,19 @@ mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 })
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String
+    name: {
+      type: String,
+      minlength: 1,
+      required: true,
+      unique: true
+      },
+    number: {
+      type: String,
+      minlength: 1,
+      required: true
+    }
   })
-  
+
   personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
       returnedObject.id = returnedObject._id.toString()
@@ -26,5 +37,7 @@ const personSchema = new mongoose.Schema({
       delete returnedObject.__v
     }
   })
+
+  personSchema.plugin(uniqueValidator)
   
   module.exports = mongoose.model('Person', personSchema)
